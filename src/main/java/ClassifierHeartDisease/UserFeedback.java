16b5b2ae56a42ef8ef0dc47202455ecc60d389d6
mvandestreek <m.van.de.streek@st.hanze.nl>
2023-11-06 21:17:30 +0100

    Added Comments to this file as well....!

--- src/main/java/ClassifierHeartDisease/UserFeedback.java
+++ src/main/java/ClassifierHeartDisease/UserFeedback.java
@@ -4,5 +4,14 @@ import weka.core.Instances;
 
 public interface UserFeedback {
 
+    /**
+     * getFeedback contract
+     * This contract has multiple implementations,
+     * because the output of the application can be different (duh...)
+     *
+     * @param data        the data
+     * @param predictions the predictions
+     * @return the feedback
+     */
     String getFeedback(Instances data, Instances predictions);
 }
